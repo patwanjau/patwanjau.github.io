9eb96d6a65900d370c2241b269eb24ca8a5ce25d
Patrick Wanjau <patwanjau@users.noreply.github.com>
2018-12-31 16:46:57 +0300

    Updates

---
+++ precache-manifest.8de14f7ec97b928396d5fc884681886e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d14a29679389db03bf5",
+    "url": "/portfolio/static/css/main.710cca39.chunk.css"
+  },
+  {
+    "revision": "7d14a29679389db03bf5",
+    "url": "/portfolio/static/js/main.7d14a296.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/portfolio/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5705ad214347677fe28946f6ba06cf2f",
+    "url": "/portfolio/index.html"
+  }
+];
