98025a19f63c40817a646bfd16fe6baa0e6d597f
Patrick Wanjau <patwanjau@users.noreply.github.com>
2019-01-04 19:47:45 +0300

    Updates

---
+++ precache-manifest.56b1a304421db3b37158ef00b682d000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba80f317568a2b2f0113",
+    "url": "/static/css/main.509a3205.chunk.css"
+  },
+  {
+    "revision": "ba80f317568a2b2f0113",
+    "url": "/static/js/main.ba80f317.chunk.js"
+  },
+  {
+    "revision": "7c77722e65ea7a8268de",
+    "url": "/static/js/1.7c77722e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b474faff32f1324e9b35238e8f0185e4",
+    "url": "/static/media/profile.b474faff.jpeg"
+  },
+  {
+    "revision": "08e57bb549eff4bf007c47c1fccc3bd9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8de14f7ec97b928396d5fc884681886e.js"
+  "/precache-manifest.56b1a304421db3b37158ef00b682d000.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
